edf4de4b2a705f22d166ba63c3709034ee49775b
Guo Guangchuan <ggc0402@qq.com>
2016-11-03 17:18:55 +0800

    update

--- src/js/home.js
+++ src/js/home.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     var erweima;
     function loadQrcode() {
         erweima = layer.open({
-            content: "<div class='close_wrapper'><img src='/static/img/erweima.png' style='width: 100%'></img><div class='close'></div><img src='/static/img/erweima_context.png' class='erweima_context'></img></div>",
+            content: "<div class='close_wrapper'><img src='/static/img/erweima.png' style='width: 100%'></img><div class='close'></div></div>",
         })
     }
 
--- static/js/section.js
+++ static/js/section.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
+    var timeID = null;
+    function slideRight() {
+        clearTimeout(timeID);
+        timeID = setTimeout(function() {
+            $.fn.fullpage.moveSlideRight()
+        }, 1500)
+    }
     $('#fullpage').fullpage({
         verticalCentered: false,
         scrollOverflow: true,
@@ -6,9 +13,11 @@ $(document).ready(function() {
         loopBottom: true,
         afterLoad: function(anchorLink, index) {
             if (index == 2) {
+                var loadedSlide = $(this);
                 $("#section2 #slide1 .person1").addClass("animated slideInDown").css("visibility", "visible")
                 $("#section2 #slide1 .person2").addClass("animated slideInUp").css("visibility", "visible")
                 $("#section2 #slide1 .name").addClass("animated slideInUp").css("visibility", "visible")
+                slideRight()
             }
             if (index == 3) {
                 $("#section3 .white-bg").addClass("animated fadeIn").css("visibility", "visible");
@@ -16,10 +25,10 @@ $(document).ready(function() {
                     $("#section3 .text-title").addClass("animated fadeIn").css("visibility", "visible");
                 }, 1000)
                 setTimeout(function() {
-                    $("#section3 .bonus").addClass("animated flipInY").css("visibility", "visible");
+                    $("#section3 .bonus").addClass("animated fadeIn").css("visibility", "visible");
                 }, 2000)
                 setTimeout(function() {
-                    $("#section3 .explain-text").addClass("animated flash").css("visibility", "visible");
+                    $("#section3 .explain-text").addClass("animated fadeIn").css("visibility", "visible");
                 }, 3000)
             }
         },
@@ -32,26 +41,31 @@ $(document).ready(function() {
                 $("#section2 #slide2 .person1").addClass("animated slideInDown").css("visibility", "visible")
                 $("#section2 #slide2 .person2").addClass("animated slideInUp").css("visibility", "visible")
                 $("#section2 #slide2 .name").addClass("animated slideInUp").css("visibility", "visible")
+                slideRight()
             }
             if (index == 2 && slideIndex == 2) {
                 $("#section2 #slide3 .person1").addClass("animated slideInDown").css("visibility", "visible")
                 $("#section2 #slide3 .person2").addClass("animated slideInUp").css("visibility", "visible")
                 $("#section2 #slide3 .name").addClass("animated slideInUp").css("visibility", "visible")
+                slideRight()
             }
             if (index == 2 && slideIndex == 3) {
                 $("#section2 #slide4 .person1").addClass("animated slideInDown").css("visibility", "visible")
                 $("#section2 #slide4 .person2").addClass("animated slideInUp").css("visibility", "visible")
                 $("#section2 #slide4 .name").addClass("animated slideInUp").css("visibility", "visible")
+                slideRight()
             }
             if (index == 2 && slideIndex == 4) {
                 $("#section2 #slide5 .person1").addClass("animated slideInDown").css("visibility", "visible")
                 $("#section2 #slide5 .person2").addClass("animated slideInUp").css("visibility", "visible")
                 $("#section2 #slide5 .name").addClass("animated slideInUp").css("visibility", "visible")
+                slideRight()
             }
             if (index == 2 && slideIndex == 5) {
                 $("#section2 #slide6 .person1").addClass("animated slideInDown").css("visibility", "visible")
                 $("#section2 #slide6 .person2").addClass("animated slideInUp").css("visibility", "visible")
                 $("#section2 #slide6 .name").addClass("animated slideInUp").css("visibility", "visible")
+                slideRight()
             }
         }
     });
@@ -61,4 +75,17 @@ $(document).ready(function() {
         $("#section1").append('<img class="animated bounceInDown board" src="static/img/section/section1/section1-board.png" />')
 
     }, 2000)
+    $("#music").on("click", function() {
+        var audio = document.getElementById("audio");;
+        if(audio.paused) {
+            audio.play();
+            $(this).addClass("rotate");
+        } else {
+            audio.pause();
+            $(this).removeClass("rotate");
+        }
+    })
+    $(".slide-down").on("click", function() {
+        $.fn.fullpage.moveSectionDown()
+    })
 })
